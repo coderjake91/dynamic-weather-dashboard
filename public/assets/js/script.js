f5aa35c3585525340568e40c625e0de9029c3a62
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-26 17:21:07 -0600

    updated the way that getWeatherData returns it's information, packaged up return object to send local information (address, geocode info, etc.) alongside onecall API fetched weatherdata

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,13 @@
-const city = 'austin'
+//Pseudocode
+/*
+-> onload, make API fetch requests, get page weather data based upon user city input
+-> break up data rendering to index.html based up use-case (current weather, 5-day forcast, city search history data-persistance)
+-> functions that handle data-rendering will be currentWeather(), fiveDayForcast(), citySearchHistory()
+
+*/
+
+
+const userCity = 'austin'
 
 //convert city string to geocode (lan, lng)
 const geoCode = async (location) =>{
@@ -12,21 +21,27 @@ const geoCode = async (location) =>{
 		});
 		const data = await response.json();
 		//fetch one city, returned data is an array of length 1 with an object having multiple properties including location
-		console.log(data.results[0].location);
-		return data.results[0].location;
+		console.log(data.results[0]);
+		return data.results[0]
 	} catch (err) {
 		console.error(err);
 	}
 }
 
 //get location-based weather data from openweather.org
-const getWeatherData = async (geoLocation) => {
-	const { lat, lng } = await geoLocation;
+const getWeatherData = async (city) => {
+	let locationData = await geoCode(city);
+
+	let { lat, lng } = locationData.location;
+
 	try{
 		const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=298d99e9901af6a77d87f19356a23cd3`)
 		
 		const data = await response.json();
-		return data;
+		return {
+			weatherInfo: data,
+			localInfo: locationData
+		};
 		
 	} catch (err) {
 		console.error(err);
@@ -34,7 +49,10 @@ const getWeatherData = async (geoLocation) => {
 
 }
 
-const coordinates = geoCode(city)
-const weatherData = getWeatherData(coordinates);
+// const renderDailyWeatherData = async () => {
+
+// }
+
+const weatherData = getWeatherData(userCity);
 
 console.log(weatherData);
