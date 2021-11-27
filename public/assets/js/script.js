6393521624c8adc4ec808ed3234bc84c19ac7539
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-27 03:32:58 -0600

    updated uv-index field on page to be color coded based upon uv-exposure scale

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,8 +2,7 @@
 /*
 -> onload, make API fetch requests, get page weather data based upon user city input
 -> break up data rendering to index.html based up use-case (current weather, 5-day forcast, city search history data-persistance)
--> functions that handle data-rendering will be currentWeather(), fiveDayForcast(), citySearchHistory()
-
+-> functions that handle data-rendering will be currentWeather(), fiveDayForecast(), citySearchHistory()
 */
 
 const userCity = 'austin'
@@ -50,19 +49,21 @@ const getWeatherData = async (city) => {
 
 //render current userCity weather to the page
 const currentWeather = async (weatherData) => {
-	let { address,
-			//current.dt Requested time, Unix, UTC 
-		  dt } = weatherData.localInfo;
+	console.log(weatherData);
+	let { address } = weatherData.localInfo;
 	let { temp,
 		  wind_speed,
 		  humidity,
 		  uvi,
-		  weather
+		  weather,
+		  dt
 		} = weatherData.weatherInfo.current;
-	console.log(address);
+
+
+		let epochUTC= new Date(dt * 1000);
 
 	//format date/time from UTC to a more readable form...
-	let date = new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short' }).format(dt);
+	let date = new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short' }).format(epochUTC);
 	//convert temp from kelvin to degrees F
 	let tempF = Math.floor((temp - 273.15) * (9/5) + 32);
 	//convert from wind speed in meters/sec to miles/hour
@@ -75,8 +76,68 @@ const currentWeather = async (weatherData) => {
 	$("#temp").html(`Temp: ${tempF}&#176 F`);
 	$("#wind").html(`Wind: ${windMPH} MPH`);
 	$("#humid").html(`Humidity: ${humidity}%`);
-	$("#uv").html(`UV Index: ${uvi}`);
-	$("#icon").html(`<img src="http://openweathermap.org/img/wn/${icon}@2x.png">${description}</img>`)
+
+	console.log(uvi);
+
+	//add uv-status indicators
+	if(uvi <= 2 ){
+		$("#uv").html(`UV Index: ${uvi}`);
+		$("#uv").addClass('badge bg-success');
+	} else if (uvi <= 5 && uvi > 2) {
+		$("#uv").html(`UV Index: ${uvi}`);
+		$("#uv").addClass('badge bg-warning');
+	} else if (uvi <= 10 && uvi > 6) {
+		$("#uv").html(`UV Index: ${uvi}`);
+		$("#uv").addClass('badge bg-danger');
+	};
+
+	$("#icon").html(`<img src="http://openweathermap.org/img/wn/${icon}@2x.png"><br>${description}</img>`)
+}
+
+const fiveDayForecast= async (weatherData) => {
+	let dailyWeather = weatherData.weatherInfo.daily;
+
+	console.log(dailyWeather);
+
+	for(let day = 0; day < 5; day++) {
+		//destructure daily forecast parameters
+		let { temp,
+			wind_speed,
+			humidity,
+			uvi,
+			weather,
+			dt
+		  } = dailyWeather[day];
+
+		  let epochUTC= new Date(dt * 1000);
+
+		//format date/time from UTC to a more readable form...
+		let date = new Intl.DateTimeFormat('en-US', {dateStyle: 'short'}).format(epochUTC);
+		console.log(date);
+		//convert temp from kelvin to degrees F
+		let tempF = Math.floor((temp.day - 273.15) * (9/5) + 32);
+		//convert from wind speed in meters/sec to miles/hour
+		let windMPH = Math.floor(wind_speed * 2.23694);
+		let [ weatherArray ] = weather;
+		let { icon, description } = weatherArray;
+		$(`#temp${day}`).html(`Temp: ${tempF}&#176 F`);
+		$(`#wind${day}`).html(`Wind: ${windMPH} MPH`);
+		$(`#humid${day}`).html(`Humidity: ${humidity}%`);
+
+		//add uv-status indicators
+		if(uvi <= 2 ){
+			$(`#uv${day}`).html(`UV Index: ${uvi}`);
+			$(`#uv${day}`).addClass('badge bg-success');
+		} else if (uvi <= 5 && uvi > 2) {
+			$(`#uv${day}`).html(`UV Index: ${uvi}`);
+			$(`#uv${day}`).addClass('badge bg-warning');
+		} else if (uvi <= 10 && uvi> 6) {
+			$(`#uv${day}`).html(`UV Index: ${uvi}`);
+			$(`#uv${day}`).addClass('badge bg-danger');
+		};
+
+		$(`#icon${day}`).html(`${date}<img src="http://openweathermap.org/img/wn/${icon}@2x.png"><br>${description}</img>`)
+	}
 }
 
 const userCitySearch = async () => {
@@ -90,6 +151,7 @@ const userCitySearch = async () => {
 			try {
 				let weatherData = await getWeatherData(userCity);
 				currentWeather(weatherData);
+				fiveDayForecast(weatherData);
 			} catch (err) {
 				console.log(err);
 			}
